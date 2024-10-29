c0a97903c3e363b407119ea2d9ee8f785eade9fd
Egor <158774538+nestluu@users.noreply.github.com>
2024-10-29 23:02:05 +0400

    fix: some bug

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ import LoginPage from './pages/LoginPage'
 export const App: React.FC = () => {
 	return (
 		<Routes>
-			<Route path='/todo-react' element={<LoginPage />} />
-			<Route path='/todo-react/home' element={<HomePage />} />
+			<Route path='/' element={<LoginPage />} />
+			<Route path='/home' element={<HomePage />} />
 		</Routes>
 	)
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -20,7 +20,7 @@ export const HomePage: React.FC = () => {
 
 	useEffect(() => {
 		if (localStorage.getItem('loggedIn') === 'false') {
-			navigate('/todo-react')
+			navigate('/')
 		}
 	}, [navigate])
 
@@ -33,7 +33,7 @@ export const HomePage: React.FC = () => {
 
 	const handleLogout = () => {
 		localStorage.setItem('loggedIn', 'false')
-		navigate('/todo-react')
+		navigate('/')
 	}
 
 	const filteredTodos = todos.filter(todo => {
