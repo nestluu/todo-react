c568815edf265ee8b97e8f81e36d80b033585459
Egor <158774538+nestluu@users.noreply.github.com>
2024-10-29 22:56:48 +0400

    add: deploy workflow

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ import LoginPage from './pages/LoginPage'
 export const App: React.FC = () => {
 	return (
 		<Routes>
-			<Route path='/login' element={<LoginPage />} />
-			<Route path='/home' element={<HomePage />} />
+			<Route path='/todo-react' element={<LoginPage />} />
+			<Route path='/todo-react/home' element={<HomePage />} />
 		</Routes>
 	)
 }
